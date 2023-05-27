0861bdc3aecf9ba70673360666d75e0c0ce24cc5
rulinma <rulinma@gmail.com>
2023-05-27 10:28:03 +0800

    添加math演示

---
+++ math/math_demo.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2023/5/27 10:25
+@Auth ： rulinma@gmail.com
+@File ：math_demo.py
+@IDE ：PyCharm
+"""
+import math
+
+if __name__ == '__main__':
+    print("\n")
+    print(math.sqrt(4))
+    print(math.floor(3.14))
+    print(math.ceil(3.14))
