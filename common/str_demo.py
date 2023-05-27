c2745122b51001b2726f6a2252647c59b0ffa439
rulinma <rulinma@gmail.com>
2023-05-27 10:30:53 +0800

    添加math演示

---
+++ common/str_demo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2023/5/27 10:29
+@Auth ： rulinma@gmail.com
+@File ：str_demo.py
+@IDE ：PyCharm
+"""
+
+if __name__ == '__main__':
+    print()
+    print("test124".upper())
