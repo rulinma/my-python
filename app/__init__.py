c700f7db2c1be977bc7f5a8437a971a904df3934
rulinma <rulinma@gmail.com>
2023-05-28 20:23:39 +0800

    添加flask

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2023/5/28 20:15
+@Auth ： rulinma@gmail.com
+@File ：__init__.py.py
+@IDE ：PyCharm
+"""
---
+++ app/app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2023/5/28 20:16
+@Auth ： rulinma@gmail.com
+@File ：app.py
+@IDE ：PyCharm
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
